4e78b0bd118154fe47ac070da6136fdd638065b3
aaldcroft <aaldcroft@myseneca.ca>
2026-09-18 20:55:10 +0000

    Completed Lab 1a

--- lab1b.py
+++ lab1b.py
@@ -9,13 +9,16 @@
 # TO-DO 1:
 #	Create a variable called "num1", take its value from user.
 num1=input("Please enter a number for num1")
+
 #	Create another variable called "num2" and take its value from user. 
 num2=input("Please enter another number for num2")
 print(type(num1))
 print(type(num2))
+
 # Convert the values to integers using int() function
 num1=int(num1)
 num2=int(num2)
+
 # TO-DO 2:
 # Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
 print("num1+num2 =",num1+num2)
